47a77030c040fff0b8763d88241d868f9d9307d4
Djonathan de Souza <13489382+dzkm@users.noreply.github.com>
2024-07-08 20:57:47 -0300

    feat/added prettier and configure eslint better

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,21 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    ignores: [
+      "node_modules/*",
+      "dist/*",
+      "*.config.js",
+      "*.d.ts",
+      "*.config.mjs",
+      "public/*",
+    ],
+  },
 );
