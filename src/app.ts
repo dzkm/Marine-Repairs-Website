f24269fb947e97a29c510d193ef4fcb2137df726
Djonathan de Souza <13489382+dzkm@users.noreply.github.com>
2024-07-08 20:41:45 -0300

    feat/added eslint

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express from 'express';
-import {Router, Request, Response} from 'express';
+import express from "express";
+import { Router, Request, Response } from "express";
 
 const app = express();
 const route = Router();
 app.use(express.json());
 
-route.get('/', (req: Request, res: Response) => {
-    res.json({message: 'hello world from Typescript'});
-})
+route.get("/", (req: Request, res: Response) => {
+  res.json({ message: "hello world from Typescript" });
+});
 
 app.use(route);
 
-app.listen(8080, () => console.log('server running on port 8080'));
+app.listen(8080, () => console.log("server running on port 8080"));
